3246467ae8ffc552b7b19075ba698eb159866102
ikaros0503 <ikaros.spacespeaker@gmail.com>
2015-09-02 02:36:54 +0700

    Update
    Thêm mục sản phẩm đang đấu giá

--- Assignment1/index.php
+++ Assignment1/index.php
@@ -10,18 +10,8 @@
 	<link rel="stylesheet" type="text/css" href="lib/bootstrap-3.3.5-dist/css/bootstrap-theme.min.css">
 	<link rel="stylesheet" type="text/css" href="lib/bootstrap-3.3.5-dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/home-style.css">
-	<script src="lib/Jssor.Slider.FullPack/js/jssor.slider.mini.js"></script>
-	<script type="text/javascript">
-		jQuery(document).ready(function ($) {
-        var options = {
-            $BulletNavigatorOptions: {
-                $Class: $JssorBulletNavigator$,
-                $ChanceToShow: 2
-            }
-        };
-        var jssor_slider1 = new $JssorSlider$('banner', options);
-    });
-	</script>
+	<link rel="stylesheet" type="text/css" href="css/count-down.css">
+	<script src="lib/jquery.countdown/jquery.downCount.js"></script>
 <body>
 <header>
 	<nav class="navbar navbar-default">
@@ -72,34 +62,88 @@
 		</div>
 	</nav>
 </header>
-<div id="main-content" class="container"> 
-	<div id="banner" style="position: relative; top: 0px; width:1170px; left: 0px; height: 300px;">
-		<div u="slides" style="cursor: move; position: absolute; overflow: hidden; left: 0px; top: 0px; width:1170px; height: 300px;">
-	        <div><img u="image" src="img/banner/banner1.jpg" /></div>
-	        <div><img u="image" src="img/banner/banner2.jpg" /></div>
-	        <div><img u="image" src="img/banner/banner3.jpg" /></div>
-    	</div>	
-    	<style>
-            .jssorb12 {
-                position: absolute;
-            }
-            .jssorb12 div, .jssorb12 div:hover, .jssorb12 .av {
-                position: absolute;
-                width: 16px;
-                height: 16px;
-                background: url(img/bullet/b21.png) no-repeat;
-                overflow: hidden;
-                cursor: pointer;
-            }
-            .jssorb12 div { background-position: -7px -7px; }
-            .jssorb12 div:hover, .jssorb12 .av:hover { background-position: -37px -7px; }
-            .jssorb12 .av { background-position: -67px -7px; }
-            .jssorb12 .dn, .jssorb12 .dn:hover { background-position: -97px -7px; }
-        </style>
-        <div align="center" u="navigator" class="jssorb12" style="bottom: 16px; right: 6px;">
-            <div u="prototype"></div>
-        </div>
-	</div>
+<div id="main-content" class="container" style="margin-top:-10px"> 
+	<div id="banner" class="carousel slide" data-ride="carousel">
+	    <ol class="carousel-indicators">
+		    <li data-target="#banner" data-slide-to="0" class="active"></li>
+		    <li data-target="#banner" data-slide-to="1"></li>
+		    <li data-target="#banner" data-slide-to="2"></li>
+	    </ol>
+	    <div class="carousel-inner" role="listbox">
+		    <div class="item active">
+		    	<img src="img/banner/banner4.jpg" alt="Chania" >
+		    </div>
+		    <div class="item">
+		        <img src="img/banner/banner5.jpg" alt="Chania" >
+		    </div>
+		    <div class="item">
+		        <img src="img/banner/banner6.jpg" alt="Flower">
+		    </div>
+	    </div>
+	    </a>
+	    <a class="right carousel-control" href="#banner" role="button" data-slide="next">
+	    </a>
+    </div>
+    <div class="product container" style="margin-top: 15px;">
+    	<aside id="current-product" class="col-md-7" style="margin-left:-15px !important">
+    		<div class="content">
+    			<aside class="product-image col-md-6" >
+    				<img src="img/products/samsung-galaxy-a8-1-400x533.png">
+    			</aside>
+	    		<aside class="details col-md-6">
+	    			<div class="product-sort-details">
+	    				<h2>Samsung Galaxy A8</h2>
+	    				<ul>
+	    					<li><h5>Màn hình FullHD</h5></li>
+	    					<li><h5>RAM 2 GB</h5></li>
+	    					<li><h5>Bộ nhớ trong 32 GB</h5></li>
+	    				</ul>
+	    			</div>
+	    			<br>
+	    			<span class="start-price" style="clear:both">
+	    				<h4><u>Giá khởi điểm</u></h4>
+	    				<h2 class="start-price">15.000.000 đ</h2>
+	    			</span>
+	    			<span class="current-price" style="display:none"></span>
+	    			<span class="time-ellapse">
+	    				<h4>Thời gian còn</h4>
+	    				<ul class="countdown">
+							<li> <span class="days">00</span>
+							<p class="days_ref">Ngày</p>
+							</li>
+							<li class="seperator">:</li>
+							<li> <span class="hours">00</span>
+							<p class="hours_ref">Giờ</p>
+							</li>
+							<li class="seperator">:</li>
+							<li> <span class="minutes">00</span>
+							<p class="minutes_ref">Phút</p>
+							</li>
+							<li class="seperator">:</li>
+							<li> <span class="seconds">00</span>
+							<p class="seconds_ref">Giây</p>
+							</li>
+						</ul>
+						<script class="source" type="text/javascript">
+						$('.countdown').downCount({
+							date: '09/09/2015 12:00:00',
+							offset: +10
+							}, function () {
+						});
+						</script>
+	    			</span>
+	    			<button class="btn btn-danger total-bet">Số lượt đấu giá <span class="badge total-bet">40</span></button>
+	    			<button class="btn btn-success bet">Tham gia đấu giá</button>
+	    		</aside>
+    		</div>
+    	</aside>
+    	<div class="vertical-separator"></div>
+    	<aside id="pass-product" class="col-md-5" >
+    		
+    	</aside>
+    </div>
 </div>
+<footer style="bcakground-color:#000">
+</footer>
 </body>
 </html>
